436726fa1f8dc8d0bbea627d3434970b93479642
Alvin KN <alvinkn.dev@gmail.com>
2023-05-24 16:32:22 +0700

    fix thread search function

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/thread/search', [ThreadController::class, 'search'])->name('thread.search');
     Route::resource('thread', ThreadController::class);
-
 });
 
 require __DIR__ . '/auth.php';
