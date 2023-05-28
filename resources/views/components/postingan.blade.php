e2b9d00f4ca6256ae6dfbc3971d1549aa3bda339
Ryzald <rizaldiwahyusaputra99@gmail.comgiadf>
2023-05-29 05:59:03 +0700

    styling profil tanpa postingan

---
+++ resources/views/components/postingan.blade.php
@@ -0,0 +1,16 @@
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="gambar.jpg" alt="Gambar Thread">
+    <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Judul Thread</div>
+        <p class="text-gray-700 text-base">Deskripsi thread yang panjang. Deskripsi thread yang panjang. Deskripsi thread
+            yang panjang.</p>
+    </div>
+    <div class="px-6 py-4 flex justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Like
+        </button>
+        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+            Comment
+        </button>
+    </div>
+</div>
